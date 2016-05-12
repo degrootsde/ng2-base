f3279032b22bfffae62a7c2335002430d4b88521
Peter Kassenaar <info@kassenaar.com>
2016-05-12 22:24:26 +0200

    Update naar @angular2.rc.1

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,12 @@
-import {Component} from 'angular2/core';
+import {Component} from '@angular/core';
 
 @Component({
 	// 1. add component description/annotations here
+	selector: 'hello-world',
+	template: `
+		<h1>Hello World!</h1>
+		<h2>Dit is Angular 2</h2>
+	`
 })
 
 export class AppComponent {
